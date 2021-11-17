b30417f6990a8d031c1e2c9ad95bab34eeb5641b
Nazim Aghayev <nazimagaev277@gmail.com>
2021-11-17 10:45:13 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <h2></h2>
+      <input />
+      <button>increment</button>
+      <button>decrement</button>
+      <button>increase</button>
+      <button>reset</button>
+    </div>
+  );
+}
+
+export default App;
